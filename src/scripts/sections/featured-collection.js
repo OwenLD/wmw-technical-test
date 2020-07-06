48adac79f53e91ff2cbd39c49b333735a5c8b5ee
Owen Lister Draper <owenlisterdraper@Owens-MacBook-Pro.local>
2020-07-06 20:30:17 +0100

    Import flickity & carousel setup

--- src/scripts/sections/featured-collection.js
+++ src/scripts/sections/featured-collection.js
@@ -7,6 +7,7 @@
  * @namespace featuredCollection
  */
 import {register} from '@shopify/theme-sections';
+import Flickity from 'flickity';
 
 /**
  * Featured collection constructor
@@ -22,4 +23,29 @@ register('featured-collection', {
   publicMethod() {
     window.console.log('Initialising featured collection section');
   },
+
+  onLoad() {
+    const carousel = document.querySelector('.featured-collection__carousel');
+    const flkty = new Flickity(carousel, {
+        cellSelector: '.product-card',
+        imagesLoaded: true,
+        wrapAround: true,
+        groupCells: true,
+        cellAlign: 'left',
+        arrowShape: { 
+          x0: 10,
+          x1: 60, y1: 50,
+          x2: 65, y2: 45,
+          x3: 20
+        }
+      }
+    );
+    
+    /**
+     * Carousel cells loading with incorrect height, this seems to fix
+     */
+    window.addEventListener('load', () => {
+      flkty.resize();
+    });
+  }
 });
